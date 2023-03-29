a586dee876114034e6f403e61528e8238c09573d
eggonz <egoitz.gonz@gmail.com>
2023-03-30 01:54:43 +0200

    wip: passive rect drawn on top of view

--- cardumen/fish.py
+++ cardumen/fish.py
@@ -4,10 +4,9 @@ import time
 
 import numpy as np
 import pygame
-import matplotlib.pyplot as plt
-import seaborn as sns
 from pygame import Vector2
 
+from cardumen import utils
 from cardumen.collision import Collider
 from cardumen.control import Action, Agent
 from cardumen.database import Table
@@ -59,13 +58,13 @@ class Fish(Entity):
         trapezoid = Polygon(self.prs, [Vector2(-1, 0), Vector2(1, 0), Vector2(6, 12), Vector2(-6, 12)],
                             fill_color=(255, 255, 255, 50), line_color=(0, 0, 0, 255))
         view = trapezoid.scale_local(h / 2).rot_local(deg2rad(90)).move_local(Vector2(w / 2 + 4, 0))
-        view = Collider(self, view, 'fish view', detect='fish body')
+        self.view = Collider(self, view, 'fish view', detect='fish body')
 
         # body rect collider
         rect = Polygon(self.prs, [Vector2(0, 0), Vector2(w, 0), Vector2(w, h), Vector2(0, h)],
                        fill_color=(0, 255, 0, 50), line_color=(0, 255, 0, 255))
         body = rect.move_local(Vector2(-w / 2, -h / 2))
-        body = Collider(self, body, 'fish body')
+        self.body = Collider(self, body, 'fish body')
 
         # proximity sense hexagon collider
         l = 100  # side length
@@ -73,29 +72,59 @@ class Fish(Entity):
         hexagon_points = [Vector2(0, -l), Vector2(a, -l / 2), Vector2(a, l / 2), Vector2(0, l),
                           Vector2(-a, l / 2), Vector2(-a, -l / 2)]
         sensor = Polygon(self.prs, hexagon_points, fill_color=(0, 0, 255, 50), line_color=(0, 0, 255, 255))
-        sensor = Collider(self, sensor, 'fish sensor', detect='fish body')
+        self.sensor = Collider(self, sensor, 'fish sensor', detect='fish body')
 
-        self.add_colliders(view, body, sensor)
+        self.add_colliders(self.view, self.body, self.sensor)
 
         # collision callbacks
-        view.on_collision_start = lambda _: view.poly.set_color(fill=(0, 255, 0, 50))
-        view.on_collision_end = lambda _: view.poly.reset_color() if not view.is_colliding() else None
-        sensor.on_collision_start = lambda _: sensor.poly.set_color(fill=(255, 0, 0, 50))
-        sensor.on_collision_end = lambda _: sensor.poly.reset_color() if not sensor.is_colliding() else None
+        # self.view.on_collision_start = lambda _: self.view.poly.set_color(fill=(0, 255, 0, 50))
+        def lam(other):
+            self.view.poly.set_color(fill=(0, 255, 0, 50))
+        self.view.on_collision_start = lam
+        self.view.on_collision_end = lambda _: self.view.poly.reset_color() if not self.view.is_colliding() else None
+        self.sensor.on_collision_start = lambda _: self.sensor.poly.set_color(fill=(255, 0, 0, 50))
+        self.sensor.on_collision_end = lambda _: self.sensor.poly.reset_color() if not self.sensor.is_colliding() else None
+
+        def draw_on_view(other: Collider):
+            if self.cat == 1:
+                # self.view_surf.blit(other.poly.get_surface(), other.parent.prs.pos - self.prs.pos)
+                surf, rect = other.poly.get_surface()
+                surf = pygame.Surface(rect.size, pygame.SRCALPHA)
+                surf.fill((255, 0, 0, 50))
+                topleft = Vector2(rect.topleft) - Vector2(self.view_rect.topleft)
+                for rep in utils.get_wraps(rect):
+                    self.view_passives.blit(surf, topleft + rep)
+        self.view.on_collision = draw_on_view
 
         # database
         self.db_table = Table(self._handler.db, f'fish{cat}')
         self.db_table.create()
 
     def update(self, dt: float) -> None:
-        super().update(dt)
+        # choose action
         action = self._agent.act(self.get_state())
         action.execute(self, dt)
+
+        # update position
         self.speed = max(self.min_speed, min(self.max_speed, self.speed))
         self.vel.from_polar((self.speed, -self.prs.rot_deg))
         self.prs.pos += self.vel * dt
 
+        # update colliders
+        _, self.view_rect = self.view.poly.get_surface()
+        self.view_passives = pygame.Surface(self.view_rect.size, pygame.SRCALPHA)
+        for collider in self.colliders:
+            collider.update(dt)
+        if self.view.is_colliding() and self.cat == 1:
+            view_surf, self.view_rect = self.view.poly.get_surface()
+            view_surf.blit(self.view_passives, (0, 0))
+            utils.plot_surf(view_surf)
+
+        # update database
         self.db_table.add(time.time(), self.get_state())
 
     def get_state(self) -> np.ndarray:
         return np.array([*self.prs.pos, *self.vel])
+
+    def __repr__(self):
+        return f'Fish({self.cat})'
